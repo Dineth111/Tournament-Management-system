a71e172dffeb0e01d19ba3d220ce59c142d80ab8
Dineth Sanjula <dinethsanjula647@gmail.com>
2025-11-15 11:08:05 +0530

    Delete server directory

--- server/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToken = (id, rememberMe = false) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: rememberMe ? process.env.JWT_EXPIRE_LONG : process.env.JWT_EXPIRE
-  });
-};
-
-module.exports = generateToken;
