94baf19f8b7bb651f60508484bffbc368cff5893
Dineth Sanjula <dinethsanjula647@gmail.com>
2025-12-14 21:21:01 +0530

    Delete frontend directory

--- frontend/src/components/ErrorBoundary.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default class ErrorBoundary extends React.Component<{children: React.ReactNode}, {hasError: boolean}> {
-  constructor(props:any){ super(props); this.state = { hasError: false }; }
-  static getDerivedStateFromError() { return { hasError: true }; }
-  componentDidCatch(error:any) { console.error('Render error:', error); }
-  render() { return this.state.hasError ? <div style={{padding:20}}>Something went wrong. Check console.</div> : this.props.children; }
-}
